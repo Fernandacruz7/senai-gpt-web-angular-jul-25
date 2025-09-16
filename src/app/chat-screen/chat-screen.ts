5331b0202c6baa6738a74fe35735e3b66288f9c8
Fernanda cruz <fehcruz002@gmail.com>
2025-09-15 21:44:56 -0300

    requisicoes da tela chat

--- src/app/chat-screen/chat-screen.ts
+++ src/app/chat-screen/chat-screen.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface IChat {
+
+  chatTitle: string;
+  id: number;
+  userId: string;
+
+}
 
 @Component({
   selector: 'app-chat-screen',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './chat-screen.html',
   styleUrl: './chat-screen.css'
 })
 export class ChatScreen {
 
+  chats: IChat[];
+
+  constructor(private http: HttpClient) { // constroi a classe
+    // iniciacao de variaveis...
+
+    this.chats = [];
+
+  }
+
+  ngOnInit() { // executado quando o angular esta pronto para rodar
+    //buscar dados da API.
+
+    this.getChats();
+
+  }
+
+  async getChats() {
+    // metodo que busca os chats da API.
+    let response = await this.http.get("https://senai-gpt-api.azurewebsites.net/chats", {
+
+      headers: {
+
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
+
+      }
+    }).toPromise();
+
+    if (response) {
+      this.chats = response as [];
+    } else {
+
+      console.log("erro ao buscar os chats");
+    }
+
+
+
+
+
+  }
 
 }
+
+
